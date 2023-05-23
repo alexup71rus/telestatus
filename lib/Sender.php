f3f580fd29410fec1e1883287a37da9e5fae556d
Александр Ходырев <a.khodyrev@intensa.ru>
2023-05-23 18:13:42 +0300

    отправка сообщений в канал

--- lib/Sender.php
+++ lib/Sender.php
@@ -9,13 +9,6 @@ Loc::loadMessages(__FILE__);
 
 class Sender
 {
-    public static $options = [
-        '%ID%' => '',
-        '%STATUS%' => '',
-        '%PREV_STATUS%' => '',
-        '%PRICE%' => '',
-    ];
-
     public static function onStatusChange(\Bitrix\Main\Event $event): bool
     {
         $module_id = pathinfo(dirname(__DIR__))["basename"];
@@ -39,14 +32,15 @@ class Sender
         $oldStatus = $event->getParameter('OLD_VALUE');
         $arOldStatus = \CSaleStatus::GetByID($oldStatus);
         $oldStatusName = $arOldStatus['NAME'];
-
-        self::$options['%ID%'] = $orderId;
-        self::$options['%STATUS%'] = $currentStatusName;
-        self::$options['%PREV_STATUS%'] = $oldStatusName;
-        self::$options['%PRICE%'] = $sum . ' Руб.';
+        $options = [
+            '%ID%' => $orderId,
+            '%STATUS%' => $currentStatusName,
+            '%PREV_STATUS%' => $oldStatusName,
+            '%PRICE%' => $sum . ' Руб.',
+        ];
 
         $request = "https://api.telegram.org/bot{$token}/sendMessage?chat_id=@{$channel}&text=";
-        $request .= self::buildTemplate($message, self::$options);
+        $request .= self::buildTemplate($message, $options);
 
         if (!file_get_contents($request)) {
             // logger
@@ -57,11 +51,12 @@ class Sender
 
     public static function buildTemplate(string $template, array $options): string
     {
+        $template = str_replace("\r", "", $template);
+        $template = str_replace("\n", "%0A", $template);
+
         foreach ($options as $key => $value) {
             $template = str_replace($key, $value, $template);
         }
-
-        $template = str_replace(PHP_EOL, "\r\n", $template);
 
         return $template;
     }
